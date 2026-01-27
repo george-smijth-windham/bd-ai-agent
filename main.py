1ddc075761e401d823cd1920972b6eb7ef08f2f4
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-01-27 08:01:10 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if None == api_key:
+        raise RuntimeError("environment variable wasn't found")
     print("Hello from bd-ai-agent!")
 
 
