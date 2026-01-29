1ba1a44a63559361fcee2402b096d5ea091d8934
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-01-29 07:23:02 +0000

    add info on token usage

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ def main():
         raise RuntimeError("environment variable wasn't found")
     print("Hello from bd-ai-agent!")
     client = genai.Client(api_key=api_key)
-    generate_content_response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    model = "gemini-2.5-flash"
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(
+        model=model,
+        contents=user_prompt,
     )
-    print(generate_content_response.text)
+    usage_metadata = response.usage_metadata
+    if usage_metadata is None:
+        raise RuntimeError("there is a problem with request")
+    prompt_token_count = usage_metadata.prompt_token_count
+    candidates_token_count = usage_metadata.candidates_token_count
+    print(
+        f"User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        f"Prompt tokens: {prompt_token_count}",
+        f"Response tokens: {candidates_token_count}",
+        f"Response:",
+        response.text,
+        sep="\n",
+    )
 
 
 if __name__ == "__main__":
