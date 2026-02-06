6c174cd7ea9e7fb0bfbb6bb618d83c2580f1a480
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-02-06 07:55:18 +0000

    update error handling

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,8 +2,18 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    abs_work_path = os.path.abspath(working_directory)
-    print(abs_work_path)
+    abs_work_dir = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_work_dir, directory))
+    valid_target_dir = os.path.commonpath([abs_work_dir, target_dir]) == abs_work_dir
+    if not valid_target_dir:
+        print(
+            f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        )
+        return
+    if not os.path.isdir(target_dir):
+        print(f'Error: "{target_dir}" is not a directory')
+        return
+    print(abs_work_dir, target_dir, valid_target_dir, sep="\n")
 
 
 get_files_info("calculator")
