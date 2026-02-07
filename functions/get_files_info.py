99a0749e0049f8be794dc3de6cdbef5d8facf64e
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-02-07 13:57:46 +0000

    wrap get_file_info body in try/catch and iterate target dir contents

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,18 +2,30 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    abs_work_dir = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(abs_work_dir, directory))
-    valid_target_dir = os.path.commonpath([abs_work_dir, target_dir]) == abs_work_dir
-    if not valid_target_dir:
-        print(
-            f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    try:
+        abs_work_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_work_dir, directory))
+        valid_target_dir = (
+            os.path.commonpath([abs_work_dir, target_dir]) == abs_work_dir
         )
-        return
-    if not os.path.isdir(target_dir):
-        print(f'Error: "{target_dir}" is not a directory')
-        return
-    print(abs_work_dir, target_dir, valid_target_dir, sep="\n")
+        if not valid_target_dir:
+            raise Exception(
+                f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            )
+        if not os.path.isdir(target_dir):
+            raise Exception(f'Error: "{target_dir}" is not a directory')
+        target_dir_contents = []
+        for path in os.listdir(target_dir):
+            name = path
+            path = os.path.join(target_dir, name)
+            size = os.path.getsize(path)
+            is_dir = os.path.isdir(path)
+            target_dir_contents.append(
+                f"- {name}: file_size={size} bytes, is_dir={is_dir}"
+            )
+        print(target_dir_contents)
+    except Exception as e:
+        return e
 
 
-get_files_info("calculator")
+print(get_files_info("calculator"))
