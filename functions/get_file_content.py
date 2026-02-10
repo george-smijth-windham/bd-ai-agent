8a1cf88c25ce55293c3928f85584c86b093018d4
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-02-10 07:48:31 +0000

    update get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,24 @@
+import os
+
+
 def get_file_content(working_directory, file_path):
-    pass
+    try:
+        abs_work_dir = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_work_dir, file_path))
+        valid_target_file = (
+            os.path.commonpath([abs_work_dir, target_file]) == abs_work_dir
+        )
+        if not valid_target_file:
+            raise Exception(
+                f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+            )
+        if not os.path.isfile(target_file):
+            raise Exception(
+                f'Error: File not found or is not a regular file: "{file_path}"'
+            )
+        return "next"
+    except Exception as e:
+        return str(e)
+
+
+print(get_file_content("calculator", "/bin/cat"))
