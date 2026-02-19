28cfd331313a550c9fca8d59d9fc4f4a33cacca4
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-02-19 18:57:42 +0000

    save

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,2 +1,15 @@
+from os import path
+
+
 def run_python_file(working_directory, file_path, args=None):
-    pass
+    try:
+        work_path = path.abspath(working_directory)
+        target_path = path.normpath(path.join(work_path, file_path))
+        valid_path = path.commonpath([work_path, target_path]) == work_path
+        if not valid_path:
+            raise Exception(
+                f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+            )
+
+    except Exception as e:
+        return str(e)
